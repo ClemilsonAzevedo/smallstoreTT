88068085127c3ae3dae9502987ce79170c4b1a56
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2024-06-10 21:38:08 +0100

    Feat: Render html on page

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,48 @@
+import { Filter, Search, ShoppingCart, Sun } from 'lucide-react'
+
 export function App() {
-	return <h1>Central App</h1>
+	return (
+		<main>
+			<header>
+				<ShoppingCart size={24} />
+				<Sun size={24} />
+			</header>
+
+			<div>
+				<label htmlFor='productName'>
+					<Search size={20} />
+				</label>
+				<input type='text' name='productName' id='productName' />
+			</div>
+
+			<div>
+				<Filter />
+				<select name='' id=''>
+					<option defaultValue={'default'} value='default'>
+						Filtre por preço
+					</option>
+				</select>
+			</div>
+
+			<div>
+				<div>
+					<img src='' alt='' />
+				</div>
+
+				<h3>Macbook Air M1 Chip M1</h3>
+
+				<p>
+					Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis tenetur
+					culpa possimus assumenda architecto praesentium rem cum.
+				</p>
+
+				<div>
+					<span>2,000.90 R$</span>
+					<button type='button'>
+						<ShoppingCart />
+					</button>
+				</div>
+			</div>
+		</main>
+	)
 }
